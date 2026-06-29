791acd22399b84bb1564829208580f47f1acc9c2
Vanshika Gaur <vanshika.gaur.2718@gmail.com>
2026-06-29 14:28:19 +0530

    Final Solutions Update in Week_4

--- OOPS/Q1/Arithmetic.java
+++
@@ -1,7 +0,0 @@
-package WEEK_4.OOPS_4.Qno_1;
-
-public class arithmatic implements test {
-    public int square(int n){
-        return n*n;
-    }
-}
--- OOPS/Q1/Test.java
+++
@@ -1,6 +0,0 @@
-package WEEK_4.OOPS_4.Qno_1;
-
-public interface test {
-    int square(int n);
-    
-}
